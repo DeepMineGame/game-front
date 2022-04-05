75e5d9834873c98c2ca48bc450e4e5e6e5f3509b
vlad <vladkhvo@gmail.com>
2022-04-05 07:30:41 +0300

    test

--- src/app/__test__/index.spec.tsx
+++ src/app/__test__/index.spec.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { screen, render } from '@testing-library/react';
 import App from '..';
 
+console.log(123)
 test('renders learn react link', () => {
     render(<App />);
     const linkElement = screen.getByText(/Loading.../i);
