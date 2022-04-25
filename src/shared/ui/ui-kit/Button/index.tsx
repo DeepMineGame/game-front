5339b12a08b2560a42a9976cf12485dabac1c309
vlad <vladkhvo@gmail.com>
2022-04-25 13:51:38 +0300

    chore: extract props

--- src/shared/ui/ui-kit/Button/index.tsx
+++ src/shared/ui/ui-kit/Button/index.tsx
@@ -2,15 +2,12 @@ import React, { MouseEventHandler, ReactNode } from 'react';
 import classNames from 'classnames';
 import styles from './styles.module.scss';
 
-export const Button = ({
-    children,
-    onClick,
-    className,
-}: {
+type Props = {
     children: ReactNode;
     onClick: MouseEventHandler;
     className: string;
-}) => {
+};
+export const Button = ({ children, onClick, className }: Props) => {
     return (
         <div onClick={onClick} className={classNames(styles.button, className)}>
             {children}
