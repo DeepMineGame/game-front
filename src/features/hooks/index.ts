708c7ed40aff3644b324afe99594879763dcdeb7
vlad <vladkhvo@gmail.com>
2022-04-25 16:03:40 +0300

    chore: fix useChainAuthContext name

--- src/features/hooks/index.ts
+++ src/features/hooks/index.ts
@@ -1,1 +1,1 @@
-export * from './useChanUser';
+export * from './useChainAuthContext';
--- src/features/hooks/useChainAuthContext.ts
+++ src/features/hooks/useChainAuthContext.ts
@@ -2,6 +2,5 @@ import { useContext } from 'react';
 import { UALContext } from 'ual-reactjs-renderer';
 
 export function useChainAuthContext() {
-    const context = useContext(UALContext);
-    return context;
+    return useContext(UALContext);
 }
