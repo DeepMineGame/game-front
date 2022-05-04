9b76c6ed031172132b3b06a5bffa7a04b7504539
vlad <vladkhvo@gmail.com>
2022-05-04 15:06:49 +0300

    chore: add dropdown

--- src/shared/ui/components/Inventory/index.tsx
+++ src/shared/ui/components/Inventory/index.tsx
@@ -1,10 +1,33 @@
-import { CardHolder, Modal } from 'shared';
+import { Button, CardHolder, Modal, Dropdown } from 'shared';
 import React, { FC } from 'react';
-import { ModalProps } from 'antd';
+import { ModalProps, Space } from 'antd';
+import { SortAscendingOutlined } from '@ant-design/icons';
+import styles from './styles.module.scss';
 
 export const Inventory: FC<ModalProps> = (props) => {
     return (
         <Modal {...props} title="Active inventory">
+            <Dropdown
+                items={[
+                    {
+                        label: 'Rarity',
+                        key: 0,
+                    },
+                    {
+                        label: 'Level',
+                        key: 1,
+                    },
+                    {
+                        label: 'Deprecation',
+                        key: 2,
+                    },
+                ]}
+            >
+                <Space className={styles.space}>
+                    <SortAscendingOutlined className={styles.icon} />
+                    <Button type="link">Sort by</Button>
+                </Space>
+            </Dropdown>
             <CardHolder onClick={() => {}} />{' '}
         </Modal>
     );
---
+++ src/shared/ui/ui-kit/Dropdown/index.tsx
@@ -0,0 +1,32 @@
+import React, { FC } from 'react';
+import { DropdownProps, Dropdown as DropdownAnt, Menu } from 'antd';
+import { ItemType } from 'antd/lib/menu/hooks/useItems';
+import styles from './styles.module.scss';
+
+type Props = {
+    items: ItemType[];
+} & Omit<DropdownProps, 'overlay'>;
+
+function enrichItemsWithDefaultCssSelector(items: ItemType[]): ItemType[] {
+    return items.map(
+        (item) =>
+            ({
+                ...item,
+                className: styles.dropdownItem,
+            } as ItemType)
+    );
+}
+
+export const Dropdown: FC<Props> = ({ children, items, ...props }) => {
+    const menu = (
+        <Menu
+            items={enrichItemsWithDefaultCssSelector(items)}
+            className={styles.dropdown}
+        />
+    );
+    return (
+        <DropdownAnt {...props} overlay={menu}>
+            {children}
+        </DropdownAnt>
+    );
+};
