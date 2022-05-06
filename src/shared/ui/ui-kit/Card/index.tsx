5a382f850ebdce35c0767ae24162346e74a0e921
vlad <vladkhvo@gmail.com>
2022-05-06 12:04:49 +0300

    chore: add card

---
+++ src/shared/ui/ui-kit/Card/index.tsx
@@ -0,0 +1,61 @@
+import React, { FC } from 'react';
+import { Progress, Tooltip } from 'antd';
+import { Logo } from 'shared';
+import cutterImg from '../../images/cutter.png';
+import styles from './styles.module.scss';
+
+type Props = {
+    imageSrc?: string;
+    initialProgress: number;
+    progressRemained: number;
+    progressCurrent: number;
+};
+
+export const Card: FC<Props> = ({
+    imageSrc,
+    initialProgress,
+    progressCurrent,
+    progressRemained,
+}) => {
+    const info = () => (
+        <div className={styles.info}>
+            {progressCurrent}/{progressRemained} ({initialProgress})
+        </div>
+    );
+    const currentProgress = (progressCurrent / progressRemained) * 100;
+    const disabledProgress = (progressRemained / initialProgress) * 100;
+    const lvlTooltip = () => (
+        <div className={styles.lvlTooltipContent}>
+            <Logo />
+            <div>120/9999</div>
+        </div>
+    );
+    const neutral4 = '#303030';
+    return (
+        <Tooltip
+            overlay={lvlTooltip}
+            className={styles.tooltip}
+            overlayClassName={styles.tooltipOverlay}
+            placement="rightTop"
+            color={neutral4}
+        >
+            <div className={styles.wrapper}>
+                <img
+                    className={styles.image}
+                    src={imageSrc || cutterImg}
+                    alt="nft-equipment-card"
+                />
+                <Progress
+                    className={styles.progress}
+                    strokeColor="#1D1D1D"
+                    percent={disabledProgress}
+                    success={{
+                        percent: currentProgress,
+                        strokeColor: '#F5C913',
+                    }}
+                    format={info}
+                />{' '}
+            </div>
+        </Tooltip>
+    );
+};
