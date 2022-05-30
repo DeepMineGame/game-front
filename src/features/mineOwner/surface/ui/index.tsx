023d4e678bbc467b5e6c4495a4e1eb6c74a8cac9
vlad <vladkhvo@gmail.com>
2022-05-30 11:11:12 +0300

    fix: move minewoner text to another file

--- src/features/mineOwner/surface/ui/index.tsx
+++ src/features/mineOwner/surface/ui/index.tsx
@@ -1,12 +1,10 @@
 import React, { FC } from 'react';
-import { Button, desktopS, Title, useMediaQuery } from 'shared';
-import { useTranslation } from 'react-i18next';
+import { desktopS, Title, useMediaQuery } from 'shared';
 import { useGate, useStore } from 'effector-react';
-import {
-    $mineOwnerCabinState,
-    MineOwnerCabinGate,
-    mineOwnerCabinState,
-} from '../../model';
+import { $mineOwnerCabinState, MineOwnerCabinGate } from '../../model';
+import { useTitles } from '../hooks/useTitles';
+import { useDescriptions } from '../hooks/useDescriptions';
+import { useLinks } from '../hooks/useLinks';
 import styles from './styles.module.scss';
 
 type Props = {
@@ -19,61 +17,20 @@ export const Surface: FC<Props> = ({ user }) => {
     });
 
     const isDesktop = useMediaQuery(desktopS);
-    const { t } = useTranslation();
     const cabinState = useStore($mineOwnerCabinState);
 
-    const titles = {
-        [mineOwnerCabinState.initial]: t('features.mineOwner.welcome'),
-        [mineOwnerCabinState.hasMineNft]: t('features.mineOwner.mineNotSet'),
-        [mineOwnerCabinState.isOutsideFromLocation]: t(
-            'features.mineOwner.mineNotSet'
-        ),
-        [mineOwnerCabinState.needSignContractWithLandLord]: t(
-            'features.mineOwner.mineNotSet'
-        ),
-        [mineOwnerCabinState.isMineActive]: t('features.mineOwner.welcome'),
-        [mineOwnerCabinState.isMineSet]: t('features.mineOwner.welcome'),
-        [mineOwnerCabinState.isMining]: t('features.mineOwner.welcome'),
-        [mineOwnerCabinState.isMineSetupInProgress]: t(
-            'features.mineOwner.progress'
-        ),
-    };
+    const titles = useTitles();
 
-    const texts = {
-        [mineOwnerCabinState.initial]: isDesktop
-            ? t('features.mineOwner.needMineCardDesktop')
-            : t('features.mineOwner.needMineCardMobile'),
-        [mineOwnerCabinState.isOutsideFromLocation]: t(
-            'features.mineOwner.needShift'
-        ),
-        [mineOwnerCabinState.needSignContractWithLandLord]: t(
-            'features.mineOwner.needLandLord'
-        ),
-        [mineOwnerCabinState.hasMineNft]: t('features.mineOwner.mineNotSet'),
-        [mineOwnerCabinState.isMineActive]: t('features.mineOwner.welcome'),
-        [mineOwnerCabinState.isMineSet]: t('features.mineOwner.welcome'),
-        [mineOwnerCabinState.isMining]: t('features.mineOwner.welcome'),
-        [mineOwnerCabinState.isMineSetupInProgress]: t(
-            'features.mineOwner.progress'
-        ),
-    };
+    const texts = useDescriptions();
+    const buttons = useLinks();
 
     return (
         <div className={styles.surface}>
-            {isDesktop && <Title fontFamily="bai">{titles[cabinState]}</Title>}
+            {isDesktop && (
+                <Title fontFamily="orbitron">{titles[cabinState]}</Title>
+            )}
             {texts[cabinState]}
-            <div>
-                <Button type="link">
-                    {isDesktop
-                        ? t('features.mineOwner.pickUpFromStorage')
-                        : t('features.mineOwner.storage')}
-                </Button>
-                <Button type="link">
-                    {isDesktop
-                        ? t('features.mineOwner.goToMarket')
-                        : t('features.mineOwner.market')}
-                </Button>
-            </div>
+            {buttons[cabinState]}
         </div>
     );
 };
