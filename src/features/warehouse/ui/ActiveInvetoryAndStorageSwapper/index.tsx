98b48cd0575324eac7c9cb0b23e6dbdae7bb0b93
vlad <vladkhvo@gmail.com>
2022-08-02 09:04:11 +0500

    fix: remove scrolling after inventory drop

--- src/features/warehouse/ui/ActiveInvetoryAndStorageSwapper/index.tsx
+++ src/features/warehouse/ui/ActiveInvetoryAndStorageSwapper/index.tsx
@@ -37,10 +37,7 @@ export const ActiveInventoryAndStorageSwapper: FC<{ accountName: string }> = ({
 
     const onDrop: DragEventHandler<HTMLDivElement> = (e) => {
         e?.preventDefault();
-        window.scrollTo({
-            top: 0,
-            behavior: 'smooth',
-        });
+
         if (draggedElement && draggedElements.has(draggedElement)) {
             return setDraggedElements(
                 removeDraggedElementFromState(draggedElement)
