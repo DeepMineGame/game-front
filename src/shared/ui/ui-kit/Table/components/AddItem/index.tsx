ebaae043767c14e3246740ed7cf505cb8b35f4aa
Vitaliy Berekchiyan <v.berekchiyan@semrush.com>
2022-08-08 10:20:04 +0200

    Fixed text in area managment table

--- src/shared/ui/ui-kit/Table/components/AddItem/index.tsx
+++ src/shared/ui/ui-kit/Table/components/AddItem/index.tsx
@@ -1,6 +1,5 @@
 import { PlusOutlined } from '@ant-design/icons';
 import React, { FC } from 'react';
-import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
 import cn from 'classnames';
 
@@ -9,12 +8,11 @@ import styles from './styles.module.scss';
 
 type Props = {
     className?: string;
-    text?: string;
+    text: string;
     onClick?: () => void;
 };
 
 export const AddItem: FC<Props> = ({ text, onClick, className }) => {
-    const { t } = useTranslation();
     const navigate = useNavigate();
     const navigateToServiceMarket = () => navigate(serviceMarket);
     return (
@@ -22,8 +20,7 @@ export const AddItem: FC<Props> = ({ text, onClick, className }) => {
             onClick={onClick || navigateToServiceMarket}
             className={cn(styles.item, className)}
         >
-            <PlusOutlined />{' '}
-            {text || t('components.common.table.addNewContractor')}
+            <PlusOutlined /> {text}
         </div>
     );
 };
