1dcfb91c108472d182551861337707a13377f94f
Aleksandr Bautin <bautin.aleksandr@latoken.com>
2022-08-11 21:53:55 +0300

    added redirect to root after logout

--- src/features/useLogout.ts
+++ src/features/useLogout.ts
@@ -1,16 +1,18 @@
 import { useContext } from 'react';
 import { UALContext } from 'ual-reactjs-renderer';
 import { useEvent } from 'effector-react';
-
+import { useNavigate } from 'react-router';
+import { root } from 'app/router/paths';
 import { logoutUserEffect } from 'entities/user';
 
-export function useLogout() {
+export const useLogout = () => {
     const { logout } = useContext(UALContext);
     const logoutUser = useEvent(logoutUserEffect);
+    const navigate = useNavigate();
 
     return async () => {
         await logoutUser();
         logout();
-        window.location.reload();
+        navigate(root);
     };
-}
+};
