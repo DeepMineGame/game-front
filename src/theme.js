18b8408b39e91a5d0df4f80bec0bac1094587c0f
Aleksandr Bautin <bautin.aleksandr@latoken.com>
2022-09-12 21:31:59 +0700

    added theme

--- src/theme.js
+++ src/theme.js
@@ -1,8 +1,111 @@
 // see default theme - https://github.com/ant-design/ant-design/blob/master/components/style/themes/default.less
 
 module.exports = {
-    '@primary-color': '#f5c913',
-
-    // buttons
+    // button
+    '@btn-danger-bg': '#A61D24',
+    '@btn-danger-color': '#FFFFFF',
+    '@btn-text-shadow': 'none',
     '@btn-primary-color': '#141414',
+    '@btn-primary-bg': '#F5C913',
+
+    '@btn-default-color': '#DBDBDB',
+    '@btn-default-bg': 'transparent',
+    '@btn-default-border': '#434343',
+
+    '@btn-default-ghost-color': '#FFFFFF',
+    '@btn-default-ghost-border': '#434343',
+
+    '@btn-text-hover-bg': '#1D1D1D',
+
+    '@btn-border-radius-base': '2px',
+    '@btn-border-radius-sm': '2px',
+
+    '@btn-font-size-lg': '16px',
+    '@btn-font-size-sm': '14px',
+    '@btn-padding-horizontal-base': '16px',
+    '@btn-padding-horizontal-lg': '16px',
+    '@btn-padding-horizontal-sm': '8px',
+
+    // allert
+    '@alert-info-border-color': '#7B6300',
+    '@alert-info-bg-color': '#3D3100',
+    '@alert-warning-border-color': '#594214',
+    '@alert-warning-bg-color': '#2B2111',
+    '@alert-warning-icon-color': '#D89614',
+    '@alert-message-color': '#DBDBDB',
+    '@alert-with-description-padding': '16px',
+    '@alert-with-description-icon-size': '21px',
+
+    // checkbox
+    '@checkbox-check-color': '#141414',
+
+    // input
+    '@input-placeholder-color': '#7D7D7D',
+    '@input-hover-border-color': '#CCA500',
+
+    // tooltip
+    '@tooltip-bg': '#434343',
+
+    // select
+    '@select-item-selected-bg': '#3D3100',
+
+    // divider
+    '@divider-color': '#434343',
+
+    // drawer
+    '@drawer-header-padding': '12px 16px 0 16px',
+    '@drawer-body-padding': '0',
+    '@drawer-bg': '#262626',
+    '@drawer-title-font-size': 'inherit',
+
+    // steps
+    '@process-tail-color': '#303030',
+
+    // badge
+    '@badge-text-color': '#FFFFFF',
+
+    // segmented
+    '@segmented-bg': '#303030',
+    '@segmented-selected-bg': '#1D1D1D',
+    '@segmented-label-color': '#DBDBDB',
+    '@segmented-hover-bg': '#303030',
+    '@segmented-label-hover-color': '#DBDBDB',
+
+    // typography
+    '@typography-title-margin-top': '0',
+    '@typography-title-margin-bottom': '0',
+    '@heading-1-size': '46px',
+    '@heading-2-size': '24px',
+    '@heading-3-size': '20px',
+    '@heading-4-size': '16px',
+    '@heading-5-size': '12px',
+    '@heading-color': '#FFFFFF',
+    '@font-family': '"Bai Jamjuree", sans-serif',
+
+    // should be in the end
+    // base
+    '@primary-color': '#F5C913',
+    '@primary-color-hover': '#CCA500',
+    '@primary-color-active': '#FFD93E',
+    '@height-base': '32px',
+    '@height-lg': '40px',
+    '@height-sm': '24px',
+    '@component-background': '#141414',
+    '@border-color-base': '#434343',
+    '@text-color': '#DBDBDB',
+    '@text-color-secondary': '#FFFFFF',
+    '@disabled-bg': '#262626',
+    '@disabled-color': '#5A5A5A',
+    '@error-color': '#D32029',
+    '@item-hover-bg': '#3D3100',
+    '@error-color-hover': '#A61D24',
+    '@error-color-active': '#D32029',
+    '@error-color-outline': 'rgba(245, 34, 45, 0.5)',
+    '@warning-color-hover': '#D89614',
+    '@warning-color-active': '#AA7714',
+    '@warning-color-outline': 'rgba(250, 173, 20, 0.5)',
+    '@outline-blur-size': '4px',
+    '@outline-width': '0',
+    '@primary-color-outline': 'rgba(24, 144, 255, 0.5)',
+    '@theme': 'variable',
 };
