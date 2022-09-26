de6907af18969bc5f7a58994462a83aa5ee38128
Aleksandr Bautin <bautin.aleksandr@latoken.com>
2022-09-26 15:49:45 +0700

    *

--- src/entities/smartcontract/tables/contracts/types.ts
+++ src/entities/smartcontract/tables/contracts/types.ts
@@ -61,7 +61,6 @@ export type ContractDto = {
     signed_by_client: number;
     signed_by_executor: number;
     cost_of_execution: number;
-    deleted_at: number;
     demand_penalty_by_client: number;
 };
 
