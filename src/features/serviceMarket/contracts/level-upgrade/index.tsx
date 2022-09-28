e11b80858578aeedd52f7717f35a441dd9819fa8
Aleksandr Bautin <bautin.aleksandr@latoken.com>
2022-09-28 21:35:08 +0700

    showing info only at contract

--- src/features/serviceMarket/contracts/level-upgrade/index.tsx
+++ src/features/serviceMarket/contracts/level-upgrade/index.tsx
@@ -1,40 +1,14 @@
 import { FC } from 'react';
 import { Row, Col } from 'antd';
-import { Alert } from 'shared';
-import { Trans } from 'react-i18next';
-import { PenaltyActions, PenaltyMessage } from 'features';
-import { ContractStatesMeta } from 'entities/smartcontract';
-import { useContractState } from 'entities/contract';
 import {
     Conditions,
     Citizen,
     Engineer,
     GeneralInfo,
 } from '../../ui/contract/level-upgrade';
-import { Completed, TerminateContract } from '../../ui/actions';
 import { ContractProps } from '../../types';
 
 const LevelUpgradeContract: FC<ContractProps> = ({ contract, accountName }) => {
-    const {
-        canTerminate,
-        showCompleted,
-        showTerminatedAlert,
-        showPenaltyActions,
-        showPenaltyMessage,
-        stateMeta,
-        isClientDemandPenalty,
-        isClientDoesntDemandPenalty,
-        isExecutorAndClientDemandPenalty,
-        isExecutorAndClientDoesntDemandPenalty,
-    } = useContractState(contract, accountName);
-
-    const penaltyMessageProps = {
-        isClientDemandPenalty,
-        isClientDoesntDemandPenalty,
-        isExecutorAndClientDemandPenalty,
-        isExecutorAndClientDoesntDemandPenalty,
-    };
-
     return (
         <Row gutter={[32, 32]}>
             <Col xs={24} md={12}>
@@ -44,42 +18,6 @@ const LevelUpgradeContract: FC<ContractProps> = ({ contract, accountName }) => {
                             contract={contract}
                             accountName={accountName}
                         />
-                    </Col>
-
-                    <Col span={24}>
-                        {showCompleted && (
-                            <Completed
-                                accountName={accountName}
-                                contractId={contract.id}
-                            />
-                        )}
-                        {showTerminatedAlert && (
-                            <Alert
-                                message={
-                                    <Trans i18nKey="pages.serviceMarket.contract.youTerminated" />
-                                }
-                                type="info"
-                                showIcon
-                            />
-                        )}
-                        {showPenaltyActions && (
-                            <PenaltyActions
-                                isViolated={
-                                    stateMeta ===
-                                        ContractStatesMeta.termViolation ||
-                                    stateMeta ===
-                                        ContractStatesMeta.deadlineViolation
-                                }
-                                amount={contract.penalty_amount}
-                                contractId={contract.id}
-                            />
-                        )}
-                        {showPenaltyMessage && (
-                            <PenaltyMessage
-                                amount={contract.penalty_amount}
-                                {...penaltyMessageProps}
-                            />
-                        )}
                     </Col>
                 </Row>
             </Col>
@@ -96,18 +34,6 @@ const LevelUpgradeContract: FC<ContractProps> = ({ contract, accountName }) => {
                             contract={contract}
                             accountName={accountName}
                         />
-                    </Col>
-
-                    <Col span={24}>
-                        <Row justify="end">
-                            {canTerminate && (
-                                <TerminateContract
-                                    penalty={contract.penalty_amount}
-                                    contractId={contract.id}
-                                    accountName={accountName}
-                                />
-                            )}
-                        </Row>
                     </Col>
                 </Row>
             </Col>
