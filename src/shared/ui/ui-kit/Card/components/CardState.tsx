a8b58c58634002abeb0d0b6825506555c8f94cf9
Alexcsmbz <41251835+Alexcsmbz@users.noreply.github.com>
2022-09-28 16:46:35 +0700

    fix: card states & remove unused equip (#210)
    * fixed Broken & Repair states showing
    
    * added removing for not in use equip

--- src/shared/ui/ui-kit/Card/components/CardState.tsx
+++ src/shared/ui/ui-kit/Card/components/CardState.tsx
@@ -1,37 +1,39 @@
+import { ToolOutlined } from '@ant-design/icons';
 import { FC } from 'react';
 import { useTranslation } from 'react-i18next';
 import {
     getTimeLeftFromUtc,
     isUtcDateExpired,
     Status,
-    Title,
     useTick,
+    Text,
+    BrokenOutlined,
 } from 'shared';
-import { ID_TO_INVENTORY, InventoryIdType } from 'entities/smartcontract';
 import styles from '../styles.module.scss';
 
 export const CardState: FC<{
-    templateId?: InventoryIdType;
     status?: Status;
     finishesAt?: number;
     onFinish?: () => void;
-}> = ({ templateId, status, finishesAt, onFinish }) => {
+}> = ({ status, finishesAt, onFinish }) => {
     const { t } = useTranslation();
     useTick(status === Status.broken);
 
     if (finishesAt && onFinish && isUtcDateExpired(finishesAt)) onFinish();
 
     return (
-        <div className={styles.cardState}>
-            <Title level={4}>
-                {status === Status.broken && !finishesAt
-                    ? `${ID_TO_INVENTORY[templateId!]} ${t(
-                          'kit.cardStates.damaged'
-                      )}`
-                    : `${t('kit.cardStates.repairing')} ${getTimeLeftFromUtc(
-                          finishesAt!
-                      )}`}
-            </Title>
+        <div className={styles.stateWrapper}>
+            <div className={styles.cardState}>
+                {status === Status.broken && !finishesAt && <BrokenOutlined />}
+                {status === Status.broken && finishesAt && (
+                    <ToolOutlined className={styles.iconTool} />
+                )}
+                <Text type="secondary">
+                    {status === Status.broken && !finishesAt
+                        ? t('kit.cardStates.broken')
+                        : getTimeLeftFromUtc(finishesAt!)}
+                </Text>
+            </div>
         </div>
     );
 };
