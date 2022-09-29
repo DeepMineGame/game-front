9880d92e4b5b39ecc0cd695cfd0f113dd1e86a94
Aleksandr Bautin <bautin.aleksandr@latoken.com>
2022-09-29 21:23:40 +0700

    fixed test

--- src/shared/lib/utils/__test__/getLabelSelectItem.test.ts
+++ src/shared/lib/utils/__test__/getLabelSelectItem.test.ts
@@ -2,29 +2,23 @@ import { getLabelSelectItem } from '../getLabelSelectItem';
 
 describe('getLabelSelectItem', () => {
     test('Function should return array of options in asc order for Select component', () => {
-        expect(getLabelSelectItem({ amount: 1, label: 'Days' })).toEqual([
-            { value: 1, label: '1 Days' },
+        expect(getLabelSelectItem({ amount: 1, label: 'Day' })).toEqual([
+            { value: 1, label: '1 Day' },
         ]);
-        expect(getLabelSelectItem({ amount: 2, label: 'Hours' })).toEqual([
-            { value: 1, label: '1 Hours' },
+        expect(getLabelSelectItem({ amount: 2, label: 'Hour' })).toEqual([
+            { value: 1, label: '1 Hour' },
             { value: 2, label: '2 Hours' },
         ]);
     });
 
     test('Function should return array of options in asc order since 0 value for Select component', () => {
-        expect(
-            getLabelSelectItem({ amount: 3, label: 'Weeks', sinceZero: true })
-        ).toEqual([
-            { value: 0, label: '0 Weeks' },
-            { value: 1, label: '1 Weeks' },
+        expect(getLabelSelectItem({ amount: 3, label: 'Week' })).toEqual([
+            { value: 1, label: '1 Week' },
             { value: 2, label: '2 Weeks' },
             { value: 3, label: '3 Weeks' },
         ]);
-        expect(
-            getLabelSelectItem({ amount: 2, label: 'Hours', sinceZero: true })
-        ).toEqual([
-            { value: 0, label: '0 Hours' },
-            { value: 1, label: '1 Hours' },
+        expect(getLabelSelectItem({ amount: 2, label: 'Hour' })).toEqual([
+            { value: 1, label: '1 Hour' },
             { value: 2, label: '2 Hours' },
         ]);
     });
