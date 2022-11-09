a4ff764203c59d89bcca6771081b2f3c4ff2b8e8
vlad <vladkhvo@gmail.com>
2022-11-09 15:16:29 +0300

    feat: add last action indicator

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -8,13 +8,7 @@ export const defaultConfig = {
 };
 
 export const isMainNet = process.env.REACT_APP_MAINNET === 'true';
-console.log(
-    process.env.REACT_APP_MAINNET === 'true',
-    process.env.REACT_APP_MAINNET,
-    typeof process.env.REACT_APP_MAINNET,
-    process.env,
-    '<-env'
-);
+console.log('upd');
 
 export const WAX_CHAIN_ID = isMainNet
     ? '1064487b3cd1a897ce03ae5b6a865651747e2e152090f99c1d19d44e01aea5a4'
