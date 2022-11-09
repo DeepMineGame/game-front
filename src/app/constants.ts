92d343c5f9f2592fea5cae47e544bcb9111baa1d
vlad <vladkhvo@gmail.com>
2022-11-09 14:22:49 +0300

    feat: debug env

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -8,6 +8,11 @@ export const defaultConfig = {
 };
 
 export const isMainNet = process.env.REACT_APP_MAINNET === 'true';
+console.log(
+    process.env.REACT_APP_MAINNET === 'true',
+    process.env.REACT_APP_MAINNET,
+    typeof process.env.REACT_APP_MAINNET
+);
 export const WAX_CHAIN_ID = isMainNet
     ? '1064487b3cd1a897ce03ae5b6a865651747e2e152090f99c1d19d44e01aea5a4'
     : 'f16b1833c747c43682f4386fca9cbb327929334a762755ebec17f6f23c9b8a12';
