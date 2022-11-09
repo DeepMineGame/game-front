58e2d522689721b54bfb727ee9778edc0f7b0809
vlad <vladkhvo@gmail.com>
2022-11-09 17:09:38 +0300

    feat: add last action indicator

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -15,7 +15,7 @@ export const WAX_CHAIN_ID = isMainNet
     : 'f16b1833c747c43682f4386fca9cbb327929334a762755ebec17f6f23c9b8a12';
 
 export const WAX_RPC_ENDPOINTS_HOST = isMainNet
-    ? 'wax.pink.gg'
+    ? 'wax.blokcrafters.io'
     : 'testnet-wax.3dkrender.com';
 
 export const WAX_RPC_ENDPOINTS_PROTOCOL = 'https';
