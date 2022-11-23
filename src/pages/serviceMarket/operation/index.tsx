57c5f8d7fc6b6661a8bbb49c9acc7ed131a6d1d8
Alexcsmbz <41251835+Alexcsmbz@users.noreply.github.com>
2022-11-23 15:55:18 +0700

    fix: order checking (#305)

--- src/pages/serviceMarket/operation/index.tsx
+++ src/pages/serviceMarket/operation/index.tsx
@@ -6,16 +6,11 @@ import {
     ContractGate,
     contractStore,
     getContractEffect,
+    useContractType as getContractType,
 } from 'entities/contract';
-import { ContractStatus } from 'entities/smartcontract';
 import { NotFoundPage } from '../../not-found';
 import { ContractPage } from './ContractPage';
 import { OrderPage } from './OrderPage';
-
-const ORDER_STATUS = [
-    ContractStatus.signed_by_client,
-    ContractStatus.signed_by_executor,
-];
 
 export const OperationPage = () => {
     const accountName = useAccountName();
@@ -26,6 +21,7 @@ export const OperationPage = () => {
     const contract = useStore(contractStore);
     const isContractLoading = useStore(getContractEffect.pending);
     const isLoading = isContractLoading || !accountName;
+    const isOrder = contract && getContractType(contract).isOrder;
 
     if (isLoading) {
         return (
@@ -39,7 +35,7 @@ export const OperationPage = () => {
         return <NotFoundPage />;
     }
 
-    if (ORDER_STATUS.includes(contract.status)) {
+    if (isOrder) {
         return <OrderPage contract={contract} accountName={accountName} />;
     }
 
