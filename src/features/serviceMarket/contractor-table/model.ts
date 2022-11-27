c5773fdb58534da5d44d487572f829c3dde4c277
Vitaliy Berekchiyan <v.berekchiyan@semrush.com>
2022-11-27 14:30:45 +0100

    Hide old orders

--- src/features/serviceMarket/contractor-table/model.ts
+++ src/features/serviceMarket/contractor-table/model.ts
@@ -1,7 +1,10 @@
 import { createGate } from 'effector-react';
 import { createEffect, createEvent, createStore, forward } from 'effector';
+import { DAY_IN_SECONDS } from 'shared';
 import { ContractDto } from 'entities/smartcontract';
 import { getMarketOrders, GetMarketOrdersParams } from 'entities/gameStat';
+
+export const MONTH_IN_SECONDS = DAY_IN_SECONDS * 30;
 
 export const ContractsGate = createGate<GetMarketOrdersParams>('ContractsGate');
 export const changeFilterEvent = createEvent<GetMarketOrdersParams>();
@@ -14,7 +17,13 @@ export const filterStore = createStore<GetMarketOrdersParams>({}).on(
 export const getContractsByFilterEffect = createEffect(getMarketOrders);
 
 export const contractsStore = createStore<null | ContractDto[]>(null)
-    .on(getContractsByFilterEffect.doneData, (_, data) => data)
+    .on(getContractsByFilterEffect.doneData, (_, data) =>
+        data.filter((order) => {
+            if (order.executor && order.client) return true;
+
+            return Date.now() / 1000 - order.create_time < MONTH_IN_SECONDS;
+        })
+    )
     .on(getContractsByFilterEffect.fail, () => null);
 
 forward({
