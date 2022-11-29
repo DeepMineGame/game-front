f3061f717e7150ef889d886b9442ca122c0735ca
vlad <vladkhvo@gmail.com>
2022-11-29 17:22:49 +0300

    feat: add engineer templates for minenet

--- src/pages/engineers-training/model/templates.ts
+++ src/pages/engineers-training/model/templates.ts
@@ -1,4 +1,4 @@
-/* eslint-disable max-classes-per-file */
+// eslint-disable-next-line max-classes-per-file
 import { isMainNet } from 'app/constants';
 import { RarityType } from 'entities/smartcontract';
 import { Level } from './types';
@@ -11,14 +11,6 @@ type RarityTemplateIdMap = {
     [RarityType.rare]: TemplateId;
     [RarityType.epic]: TemplateId;
     [RarityType.legendary]: TemplateId;
-};
-
-const mockTemplates: RarityTemplateIdMap = {
-    [RarityType.common]: 0,
-    [RarityType.uncommon]: 0,
-    [RarityType.rare]: 0,
-    [RarityType.epic]: 0,
-    [RarityType.legendary]: 0,
 };
 
 enum Network {
@@ -44,7 +36,13 @@ class EquipmentTemplates {
                 [RarityType.epic]: 522373,
                 [RarityType.legendary]: 522374,
             },
-            [Network.mainNet]: mockTemplates,
+            [Network.mainNet]: {
+                [RarityType.common]: 619375,
+                [RarityType.uncommon]: 619386,
+                [RarityType.rare]: 619395,
+                [RarityType.epic]: 619405,
+                [RarityType.legendary]: 619418,
+            },
         },
         [Level.second]: {
             [Network.testNet]: {
@@ -54,7 +52,13 @@ class EquipmentTemplates {
                 [RarityType.epic]: 524292,
                 [RarityType.legendary]: 524300,
             },
-            [Network.mainNet]: mockTemplates,
+            [Network.mainNet]: {
+                [RarityType.common]: 619376,
+                [RarityType.uncommon]: 619387,
+                [RarityType.rare]: 619396,
+                [RarityType.epic]: 619406,
+                [RarityType.legendary]: 619420,
+            },
         },
         [Level.third]: {
             [Network.testNet]: {
@@ -64,7 +68,13 @@ class EquipmentTemplates {
                 [RarityType.epic]: 524293,
                 [RarityType.legendary]: 524301,
             },
-            [Network.mainNet]: mockTemplates,
+            [Network.mainNet]: {
+                [RarityType.common]: 619377,
+                [RarityType.uncommon]: 619388,
+                [RarityType.rare]: 619397,
+                [RarityType.epic]: 619408,
+                [RarityType.legendary]: 619421,
+            },
         },
         [Level.fourth]: {
             [Network.testNet]: {
@@ -74,7 +84,13 @@ class EquipmentTemplates {
                 [RarityType.epic]: 524294,
                 [RarityType.legendary]: 524302,
             },
-            [Network.mainNet]: mockTemplates,
+            [Network.mainNet]: {
+                [RarityType.common]: 619378,
+                [RarityType.uncommon]: 619389,
+                [RarityType.rare]: 619398,
+                [RarityType.epic]: 619409,
+                [RarityType.legendary]: 619422,
+            },
         },
         [Level.fifth]: {
             [Network.testNet]: {
@@ -84,7 +100,13 @@ class EquipmentTemplates {
                 [RarityType.epic]: 524295,
                 [RarityType.legendary]: 524303,
             },
-            [Network.mainNet]: mockTemplates,
+            [Network.mainNet]: {
+                [RarityType.common]: 619379,
+                [RarityType.uncommon]: 619390,
+                [RarityType.rare]: 619399,
+                [RarityType.epic]: 619410,
+                [RarityType.legendary]: 619424,
+            },
         },
         [Level.sixth]: {
             [Network.testNet]: {
@@ -94,7 +116,13 @@ class EquipmentTemplates {
                 [RarityType.epic]: 524296,
                 [RarityType.legendary]: 524304,
             },
-            [Network.mainNet]: mockTemplates,
+            [Network.mainNet]: {
+                [RarityType.common]: 619380,
+                [RarityType.uncommon]: 619391,
+                [RarityType.rare]: 619400,
+                [RarityType.epic]: 619411,
+                [RarityType.legendary]: 619425,
+            },
         },
         [Level.seventh]: {
             [Network.testNet]: {
@@ -104,7 +132,13 @@ class EquipmentTemplates {
                 [RarityType.epic]: 524297,
                 [RarityType.legendary]: 524305,
             },
-            [Network.mainNet]: mockTemplates,
+            [Network.mainNet]: {
+                [RarityType.common]: 619381,
+                [RarityType.uncommon]: 619392,
+                [RarityType.rare]: 619401,
+                [RarityType.epic]: 619412,
+                [RarityType.legendary]: 619426,
+            },
         },
         [Level.eighth]: {
             [Network.testNet]: {
@@ -114,7 +148,13 @@ class EquipmentTemplates {
                 [RarityType.epic]: 524298,
                 [RarityType.legendary]: 524306,
             },
-            [Network.mainNet]: mockTemplates,
+            [Network.mainNet]: {
+                [RarityType.common]: 619382,
+                [RarityType.uncommon]: 619393,
+                [RarityType.rare]: 619402,
+                [RarityType.epic]: 619413,
+                [RarityType.legendary]: 619428,
+            },
         },
         [Level.ninth]: {
             [Network.testNet]: {
@@ -124,7 +164,13 @@ class EquipmentTemplates {
                 [RarityType.epic]: 524299,
                 [RarityType.legendary]: 524307,
             },
-            [Network.mainNet]: mockTemplates,
+            [Network.mainNet]: {
+                [RarityType.common]: 619383,
+                [RarityType.uncommon]: 619394,
+                [RarityType.rare]: 619403,
+                [RarityType.epic]: 619414,
+                [RarityType.legendary]: 619429,
+            },
         },
     };
 
@@ -149,39 +195,39 @@ class MineTemplates {
     > = {
         [Level.first]: {
             [Network.testNet]: 522376,
-            [Network.mainNet]: 0,
+            [Network.mainNet]: 619366,
         },
         [Level.second]: {
             [Network.testNet]: 524134,
-            [Network.mainNet]: 0,
+            [Network.mainNet]: 619367,
         },
         [Level.third]: {
             [Network.testNet]: 524198,
-            [Network.mainNet]: 0,
+            [Network.mainNet]: 619368,
         },
         [Level.fourth]: {
             [Network.testNet]: 524199,
-            [Network.mainNet]: 0,
+            [Network.mainNet]: 619369,
         },
         [Level.fifth]: {
             [Network.testNet]: 524200,
-            [Network.mainNet]: 0,
+            [Network.mainNet]: 619370,
         },
         [Level.sixth]: {
             [Network.testNet]: 524201,
-            [Network.mainNet]: 0,
+            [Network.mainNet]: 619371,
         },
         [Level.seventh]: {
             [Network.testNet]: 524202,
-            [Network.mainNet]: 0,
+            [Network.mainNet]: 619372,
         },
         [Level.eighth]: {
             [Network.testNet]: 524203,
-            [Network.mainNet]: 0,
+            [Network.mainNet]: 619373,
         },
         [Level.ninth]: {
             [Network.testNet]: 524204,
-            [Network.mainNet]: 0,
+            [Network.mainNet]: 619374,
         },
     };
 
