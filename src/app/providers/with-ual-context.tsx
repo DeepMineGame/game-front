07787439fa16cfedf0ca6847bab81c5dbf00a930
vlad <vladkhvo@gmail.com>
2022-12-20 18:12:52 +0300

    test with wombat

--- src/app/providers/with-ual-context.tsx
+++ src/app/providers/with-ual-context.tsx
@@ -2,20 +2,34 @@ import React from 'react';
 import { UALProvider } from 'ual-reactjs-renderer';
 import { Anchor } from 'ual-anchor';
 import { Wax } from '@eosdacio/ual-wax';
-import { isMainNet, waxChain } from '../constants';
+import { Wombat } from 'wombat-ual';
+import { isMainNet, WAX_RPC_ENDPOINTS_HOST, waxChain } from '../constants';
 
+const exampleNet = {
+    chainId: 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906',
+    // name: 'EOS Mainnet',
+    rpcEndpoints: [
+        {
+            protocol: 'https',
+            host: WAX_RPC_ENDPOINTS_HOST,
+            port: 443,
+        },
+    ],
+};
+const wombat = new Wombat([exampleNet], { appName: 'DeepMine' });
 const chains = [waxChain];
+
 const anchor = new Anchor(chains, { appName: 'DeepMine' });
 const waxCloudWallet = new Wax(chains);
 
 export const waxAuthenticators = isMainNet
-    ? [anchor, waxCloudWallet]
+    ? [anchor, waxCloudWallet, wombat]
     : [anchor];
 
 export const withUalContext = (App: React.FC) => () =>
     (
         <UALProvider
-            chains={chains}
+            chains={[...chains, wombat]}
             appName="DeepMine"
             authenticators={waxAuthenticators}
         >
