3d0b31869981b57e6fcf9a7bab31c09b59a190d7
Vitaliy Berekchiyan <v.berekchiyan@semrush.com>
2022-12-29 15:26:36 +0100

    Fixed error wneh user made logout

--- src/features/useLogout.ts
+++ src/features/useLogout.ts
@@ -1,18 +1,15 @@
 import { useContext } from 'react';
 import { UALContext } from 'ual-reactjs-renderer';
 import { useEvent } from 'effector-react';
-import { useNavigate } from 'react-router';
-import { root } from 'app/router/paths';
 import { logoutUserEffect } from 'entities/user';
 
 export const useLogout = () => {
     const { logout } = useContext(UALContext);
     const logoutUser = useEvent(logoutUserEffect);
-    const navigate = useNavigate();
 
     return async () => {
         await logoutUser();
         logout();
-        navigate(root);
+        window.location.reload();
     };
 };
