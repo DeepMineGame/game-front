bf160fc430323db2fc1ddef177fdcb6f9192a572
vlad <vladkhvo@gmail.com>
2023-01-26 12:07:13 +0300

    feat: add wombat

--- src/app/providers/with-ual-context.tsx
+++ src/app/providers/with-ual-context.tsx
@@ -2,14 +2,17 @@ import React from 'react';
 import { UALProvider } from 'ual-reactjs-renderer';
 import { Anchor } from 'ual-anchor';
 import { Wax } from '@eosdacio/ual-wax';
+import { Wombat } from 'wombat-ual';
 import { isMainNet, waxChain } from '../constants';
 
 const chains = [waxChain];
 const anchor = new Anchor(chains, { appName: 'DeepMine' });
 const waxCloudWallet = new Wax(chains);
 
+const wombat = new Wombat([waxChain], { appName: 'DeepMine' });
+
 export const waxAuthenticators = isMainNet
-    ? [anchor, waxCloudWallet]
+    ? [anchor, waxCloudWallet, wombat]
     : [anchor];
 
 export const withUalContext = (App: React.FC) => () =>
