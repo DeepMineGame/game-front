5116aa117241af712a05fe7a756cca88d0c02397
vlad <vladkhvo@gmail.com>
2023-02-27 13:09:01 +0300

    fix: made calculation for fee_percent when create contract

--- src/entities/smartcontract/actions/crtcontype.ts
+++ src/entities/smartcontract/actions/crtcontype.ts
@@ -12,7 +12,6 @@ const getBaseFields = (orderData: BaseOrder) => {
         opt_executor: orderData.opt_executor,
         opt_asset_id: Number.isNaN(assetId) ? undefined : assetId,
         is_client: isClient,
-        penalty_amount: Number(orderData.penalty_amount || 0) * 10 ** 8,
         deadline_duration:
             orderData.deadline_duration_in_days * DAY_IN_SECONDS +
             orderData.deadline_duration_in_hours * HOUR_IN_SECONDS,
@@ -34,7 +33,7 @@ export const createMineOrder = (orderData: MineOrder) => {
                 data: {
                     ...getBaseFields(orderData),
                     contract_type: orderData.contract_type,
-                    fee_percent: Number(orderData.fee_percent),
+                    fee_percent: Number(orderData.fee_percent || 0) * 10 ** 8,
                     contract_duration:
                         orderData.contract_duration * DAY_IN_SECONDS,
                     opt_level: orderData.opt_level ?? null,
@@ -65,6 +64,8 @@ export const createLevelUpgradeOrder = (orderData: LevelUpgradeOrder) => {
                     opt_level: orderData.opt_level,
                     opt_rarity: orderData.opt_rarity,
                     cost_of_execution: Number(orderData.cost_of_execution),
+                    penalty_amount:
+                        Number(orderData.penalty_amount || 0) * 10 ** 8,
                 },
             },
         ],
