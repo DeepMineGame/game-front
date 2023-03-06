dc178c76fd34ae91489197fdf2995576fd3dca7a
vlad <vladkhvo@gmail.com>
2023-03-06 11:49:05 +0300

    feat: add autorenew_enabled for self signed contract

--- src/entities/smartcontract/actions/crtcontype.ts
+++ src/entities/smartcontract/actions/crtcontype.ts
@@ -13,6 +13,9 @@ const getBaseFields = (orderData: BaseOrder) => {
         opt_asset_id: Number.isNaN(assetId) ? undefined : assetId,
         is_client: isClient,
         deposit: Number(orderData.deposit || 0) * 10 ** 8,
+        deadline_duration:
+            orderData.deadline_duration_in_days * DAY_IN_SECONDS +
+            orderData.deadline_duration_in_hours * HOUR_IN_SECONDS,
     };
 };
 
@@ -61,9 +64,6 @@ export const createLevelUpgradeOrder = (orderData: LevelUpgradeOrder) => {
                     opt_level: orderData.opt_level,
                     opt_rarity: orderData.opt_rarity,
                     cost_of_execution: Number(orderData.cost_of_execution),
-                    deadline_duration:
-                        orderData.deadline_duration_in_days * DAY_IN_SECONDS +
-                        orderData.deadline_duration_in_hours * HOUR_IN_SECONDS,
                 },
             },
         ],
