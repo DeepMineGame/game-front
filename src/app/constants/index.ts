275c47850c31e30cc02238296991786273fbeb65
vlad <vladkhvo@gmail.com>
2023-03-28 17:29:14 +0300

    fix: return our prod node

--- src/app/constants/index.ts
+++ src/app/constants/index.ts
@@ -17,9 +17,8 @@ export const WAX_CHAIN_ID = isMainNet
     : 'f16b1833c747c43682f4386fca9cbb327929334a762755ebec17f6f23c9b8a12';
 
 export const WAX_RPC_ENDPOINTS_HOST = isMainNet
-    ? 'wax.greymass.com'
-    : // 'node-prod.deepmine.world'
-      'node.deepmine.world';
+    ? 'node-prod.deepmine.world'
+    : 'node.deepmine.world';
 
 export const WAX_RPC_ENDPOINTS_PROTOCOL = 'https';
 
