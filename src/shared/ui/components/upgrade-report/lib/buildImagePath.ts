9a7de64960709911f1c68ecda00c73ec5fbecb75
vlad <vladkhvo@gmail.com>
2023-04-07 13:43:25 +0300

    fix: make correct for one asset engineer upgrade

--- src/shared/ui/components/upgrade-report/lib/buildImagePath.ts
+++ src/shared/ui/components/upgrade-report/lib/buildImagePath.ts
@@ -5,9 +5,13 @@ export const buildImagePath = (contract: ContractDto) => {
     const assetIds = contract.attrs.find(
         ({ key }) => key === 'asset_ids'
     )?.value;
+    const assetTemplateId = contract.attrs.find(
+        ({ key }) => key === 'asset_template_id'
+    )?.value;
     const isMultiAsset = assetIds?.includes(',');
+    console.log(isMultiAsset, assetIds, getImagePath(Number(assetIds)));
     if (isMultiAsset) {
         return commonAssetsSetImg;
     }
-    return getImagePath(Number(assetIds));
+    return getImagePath(Number(assetTemplateId));
 };
