dc94135c76131e93e7c106ff53adaed4c3f38cbb
vlad <vladkhvo@gmail.com>
2023-04-10 15:00:15 +0300

    fix: set required button for engineer contract

--- src/features/engineer/equipment-upgrade/ui/get-report/index.tsx
+++ src/features/engineer/equipment-upgrade/ui/get-report/index.tsx
@@ -1,18 +1,9 @@
 import { FC } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useSmartContractAction } from 'features/hooks';
-import {
-    ContractDto,
-    LOCATION_TO_ID,
-    upgradeFinish,
-} from 'entities/smartcontract';
+import { ContractDto, upgradeFinish } from 'entities/smartcontract';
 import { Button } from 'shared/ui/ui-kit';
-import {
-    useAccountName,
-    useReloadPage,
-    useTravelConfirm,
-    useUserLocation,
-} from 'shared/lib/hooks';
+import { useAccountName, useReloadPage } from 'shared/lib/hooks';
 import { UpgradeReport } from 'shared/ui';
 
 type Props = {
@@ -23,10 +14,6 @@ const GetUpgrade: FC<Props> = ({ contract }) => {
     const accountName = useAccountName();
     const { t } = useTranslation();
     const reloadPage = useReloadPage();
-    const inLocation = useUserLocation();
-    const { travelConfirm } = useTravelConfirm(
-        LOCATION_TO_ID.engineers_workshop
-    );
     const signUpgradeFinish = useSmartContractAction({
         action: upgradeFinish(accountName, contract.id),
         onSignSuccess: () => setTimeout(reloadPage, 1500),
@@ -37,11 +24,7 @@ const GetUpgrade: FC<Props> = ({ contract }) => {
     };
 
     const handleUpgradeFinish = () => {
-        if (inLocation.engineersWorkshop) {
-            finishUpgrade();
-        } else {
-            travelConfirm(reloadPage);
-        }
+        finishUpgrade();
     };
 
     if (!contract.deleted_at) {
