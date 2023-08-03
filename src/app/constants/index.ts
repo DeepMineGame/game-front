7ff53a0908fd830bf7dd3657d9f3bbb1c54ef142
Vladislav Khvostov <vladkhvo@gmail.com>
2023-08-03 12:08:56 +0300

    Update index.ts

--- src/app/constants/index.ts
+++ src/app/constants/index.ts
@@ -57,8 +57,8 @@ const defaultEndpoints = {
           ],
     atomic: isMainNet
         ? [
-              'https://api-wax-aa.eosarabia.net/atomicassets/v1',
               'https://atomic.ledgerwise.io/atomicassets/v1',
+              'https://api-wax-aa.eosarabia.net/atomicassets/v1',
               'https://wax-aa.eu.eosamsterdam.net/atomicassets/v1',
               'https://wax.blokcrafters.io/atomicassets/v1',
               'https://aa.dapplica.io/atomicassets/v1',
