caebcb616545fc7f8e2999e9562dc38d1919169e
Vladislav Khvostov <vladkhvo@gmail.com>
2023-09-07 11:32:53 +0300

    Update atomic node

--- src/app/constants/index.ts
+++ src/app/constants/index.ts
@@ -57,7 +57,7 @@ const defaultEndpoints = {
           ],
     atomic: isMainNet
         ? [
-              'https://atomic.ledgerwise.io/atomicassets/v1',
+              'https://api.atomic.greeneosio.com/atomicassets/v1',
               'https://api-wax-aa.eosarabia.net/atomicassets/v1',
               'https://wax-aa.eu.eosamsterdam.net/atomicassets/v1',
               'https://wax.blokcrafters.io/atomicassets/v1',
@@ -69,7 +69,7 @@ const defaultEndpoints = {
               'https://wax-aa.eosdublin.io/atomicassets/v1',
               'https://atomic.wax.eosrio.io/atomicassets/v1',
               'https://atomic.hivebp.io/atomicassets/v1',
-              'https://api.atomic.greeneosio.com/atomicassets/v1',
+              'https://atomic.ledgerwise.io/atomicassets/v1',
               'https://atomic.sentnl.io/atomicassets/v1',
               'https://aa-api-wax.eosauthority.com/atomicassets/v1',
               'https://atomic.tokengamer.io/atomicassets/v1',
